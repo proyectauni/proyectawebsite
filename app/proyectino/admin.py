5de6a48a64c23984a41e2e779d4aebf6f9dde95f
gignaciov <gignaciov@uni.pe>
2019-11-09 17:49:37 -0500

    trabajo de proyecto y proyectino apps

--- app/proyectino/admin.py
+++ app/proyectino/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class Certificacion_proyectinoInLine(admin.TabularInline):
+    model = Certificacion_proyectino
+
+class ProyectinoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'lastname', 'email', 'celular', 'fecha_ingreso', 'fecha_nacimiento', 'cantidad_toribonus', 'estado')
+    list_filter = ('estado',)
+    search_fields = ('name','lastname')
+    ordering = ('name','lastname')
+    inlines = [Certificacion_proyectinoInLine]
+
+admin.site.register(Proyectino, ProyectinoAdmin)
+admin.site.register(Especialidad)
+admin.site.register(Tipo_toribonu)
+admin.site.register(Toribonu)
