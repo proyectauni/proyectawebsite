895a396a01184e1327b093b212177bb88749a519
gignaciov <gignaciov@uni.pe>
2020-01-25 18:47:43 -0500

    corrigiendo staticos

--- pagwebproyecta/settings.py
+++ pagwebproyecta/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static_prod'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
