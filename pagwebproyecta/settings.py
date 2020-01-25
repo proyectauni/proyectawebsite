0d8b1a5225090817016822ec6957c8462105e3cb
gignaciov <gignaciov@uni.pe>
2020-01-25 18:55:09 -0500

    corrigiendo estaticos

--- pagwebproyecta/settings.py
+++ pagwebproyecta/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/app/landing/static/'
 STATIC_ROOT = 'static_prod'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
